d746581d407dd1fdd8c4499d956ee6bab8bda786
CesarRam <Cesar.Ram.30020@gmail.com>
2024-05-16 16:57:01 -0600

    Ajuste

--- src/views/servicios/servicioSocial.vue
+++ src/views/servicios/servicioSocial.vue
@@ -101,7 +101,7 @@
               <section class="row">
                 <h4 class="text-center">Proceso</h4>
                 <div class="col-md-6 text-center">
-                  <img style="width: auto; height: 250px;" src="../../assets/images/oficio.png" alt="tu navegador no soporta imagenes">
+                  <img style="width: auto; height: 250px;" src="../../assets/images/oficio.PNG" alt="tu navegador no soporta imagenes">
                 </div>
 
                 <div class="col-md-6">
